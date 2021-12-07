898e5a0e3171db1825abe6d802ce6e26971d74dd
xuxiaoman7 <xuxiaoman7@163.com>
2021-12-07 16:35:18 +0800

    增加了数据库的相关操作

--- SE2/myWeb/views.py
+++ SE2/myWeb/views.py
@@ -9,6 +9,7 @@ from distutils.command import register
 from django.shortcuts import render, reverse, redirect
 from django.http import HttpResponse
 from myWeb.models import Person
+from django.contrib import messages
 
 def index(request):
    return render(request,"index.html")
@@ -28,7 +29,6 @@ def Login(request):
             now_password = password
             db_password = e.password
             if now_password == db_password:
-                #respone = render(request,'index.html')
                 if type=='citizen':
                     respone = redirect('/Citizen/')
                 if type == 'government':
@@ -48,18 +48,31 @@ def Citizen(request):
         hole_street = request.POST.get("hole_street")
         hole_location = request.POST.get("hole_location")
         hole_size = request.POST.get("hole_size")
+        hole_size_n = int(hole_size)
         #获取当前hole表的数据，为hole分配id
-        hole_id = ""
+        hole_count = Hole.objects.all().count()
+        hole_id = str(hole_count+1)
         #根据street确定distinct
         hole_distinct = ""
+        if(hole_street=="内环东路"):
+            hole_distinct = "番禺区"
+        elif(hole_street=="北京路"):
+            hole_distinct = "越秀区"
+        else:
+            hole_distinct = "Unclear"
         #根据size确定priority
         #历遍整个表找出有多少个size大于等于它的，假设有n个，那么priority就是n+1
-        hole_priority = 2
+        size_count = Hole.objects.filter(hole_size__gte=hole_size_n).count()
+        hole_priority = size_count + 1
         #自动填充当前hole的hole status
         hole_status = "waiting_for_repair"
+        #开始存入表中数据
         hole1 = Hole(hole_id = hole_id,hole_street = hole_street,hole_distinct= hole_distinct,hole_location = hole_location,hole_size = int(hole_size),hole_priority = hole_priority)
         hole1.save()
-        return HttpResponse("<p>Add Successfully!</p>")
+        #弹窗设置
+        messages.success(request,'Save Successfully!')
+        return render(request, 'citizen.html')
+
 
 
 def Government(request):
